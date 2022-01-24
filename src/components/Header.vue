282fc5a4c61384a25facbd3d403439a549eaed42
hu100 <84121935+epath2c@users.noreply.github.com>
2022-01-24 09:48:53 +0800

    how to use simple props in vue

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,7 +1,7 @@
 <template>
   <!-- Content in this component, things you will export -->
   <header>
-    <h1>Task Tracker</h1>
+    <h1>{{ title }}</h1>
   </header>
 </template>
 
@@ -9,6 +9,18 @@
 // * JS logic about this component, define export first
 export default {
   name: 'Header',
+  //   props: ['title'],
+  //   props: {
+  //     title: String,
+  //   },
+  props: {
+    title: {
+      type: String,
+      default: 'Hello World',
+      //   When you don't have a value for title, the default value will take in charge.
+    },
+    // * These three ways of defining props are all ok.
+  },
 };
 </script>
 
