7ac9eedff5691976982bfeb03098c44ef7634cd4
hu100 <84121935+epath2c@users.noreply.github.com>
2022-01-24 09:42:54 +0800

    basic import and export

---
+++ src/components/Header.vue
@@ -0,0 +1,23 @@
+<template>
+  <!-- Content in this component, things you will export -->
+  <header>
+    <h1>Task Tracker</h1>
+  </header>
+</template>
+
+<script>
+// * JS logic about this component, define export first
+export default {
+  name: 'Header',
+};
+</script>
+
+<style scoped>
+/* scoped means anything in here will be only used for this component particular. */
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 20px;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
 
-createApp(App).mount('#app')
+createApp(App).mount('#app');
+// * 把app里的template放进id为app的div里面
